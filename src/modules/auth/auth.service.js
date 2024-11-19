22c04a90ff3783e47fc7ce02a2bcf033a0111ad3
ali pejman <alip.web.d@gmail.com>
2024-11-19 14:51:05 +0330

    first step for user auth

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,13 @@
+class authService {
+    async sendOTP(mobile) {
+       
+    }
+    async checkOTP(mobile, code) {
+       
+    }
+    async logout() {
+       
+    }
+}
+
+module.exports = new authService()
