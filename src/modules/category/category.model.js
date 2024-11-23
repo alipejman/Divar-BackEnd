e7bf7f04a5cb10f3ed89ce00462524fb214cf586
ali pejman <alip.web.d@gmail.com>
2024-11-23 10:57:14 +0330

    create Category Swagger

--- src/modules/category/category.model.js
+++ src/modules/category/category.model.js
@@ -3,10 +3,10 @@ const { Types, Schema, model } = require("mongoose");
 const categorySchema = new Schema({
     name: {type: String, required: true},
     slug: {type: String, required: true, index: true},
-    icon: {type: String, require: true},
+    icon: {type: String, required: true},
     parent: {type: Types.ObjectId, ref: 'Category', required: false},
     parents: {type: Types.ObjectId, ref: 'Category', required: false, default: []}
-}, {virtuals: true, versionKey: false, id: false});
+}, {versionKey: false, id: false, toJSON: {virtuals: true}});
 categorySchema.virtual('children', {
     ref: 'category',
     localField: '__id',
