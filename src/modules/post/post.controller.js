a7fb03b63e3ff3346da090dbe01b092a7e2be027
ali pejman <alip.web.d@gmail.com>
2024-11-30 09:25:48 +0330

    get options in craete post

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -1,18 +1,40 @@
 const autoBind = require("auto-bind");
-const postService = require("./post.service");
 const StatusCodes = require('http-codes');
-const { postMessage } = require("./post.message");
+const  postMessage  = require("./post.message");
+const categoryModel = require("../category/category.model");
+const createHttpError = require("http-errors");
+const postService = require("./post.service");
 
 class postController {
     #service;
+    #categoryModel;
     constructor() {
         autoBind(this);
         this.#service = postService;
+        this.#categoryModel = categoryModel;
     }
 
     async createPostPage(req, res, next) {
         try {
-           res.render('./pages/admin/create-post')
+            let {slug} = req.query;
+            let showBack = false;
+            let match = {parent: null};
+            let options;
+            if(slug) {
+                slug = slug.trim();
+                const category = await this.#categoryModel.findOne({slug});
+                if (!category) throw new createHttpError.NotFound(postMessage.NotFound);
+                options = await this.#service.getCategoryOptions(category._id);
+                if(options.length === 0) options = null;
+                showBack = true;
+                match = {
+                    parent: category._id
+                };
+            }
+            const categories = await this.#categoryModel.aggregate([{
+                $match: match
+            }]);
+           res.render('./pages/admin/create-post', {categories, showBack, options})
         } catch (error) {
             next(error);
         }
--- src/modules/post/post.service.js
+++ src/modules/post/post.service.js
@@ -4,15 +4,19 @@ const { isValidObjectId, Types, set } = require("mongoose");
 const createHttpError = require("http-errors");
 const slugify = require("slugify");
 const { postMessage } = require("./post.message");
+const optionModel = require("../option/option.model");
 
 class postService {
   #model;
+  #optionModel;
   constructor() {
     autoBind(this);
     this.#model = postModel;
+    this.#optionModel = optionModel;
   }
-  async create() {
-   
+  async getCategoryOptions(categoryId) {
+    const options = await this.#optionModel.find({category: categoryId});
+    return options;
   }
 }
 
